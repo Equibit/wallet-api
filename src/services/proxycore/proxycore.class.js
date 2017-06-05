c681b834bb3c028f02cb5d48cafa7d261b910513
Ilya Fadeev <fadeev.ilya@gmail.com>
2017-06-04 23:57:58 -0400

    added a proxy to local core node

---
+++ src/services/proxycore/proxycore.class.js
@@ -0,0 +1,58 @@
+const axios = require('axios')
+
+class Service {
+  constructor (options) {
+    this.options = options || {}
+  }
+
+  find (params) {
+    return axios({
+      method: 'POST',
+      // url: 'http://99.227.230.43:8331',
+      url: 'http://localhost:18332',
+      data: {
+        jsonrpc: '1.0',
+        method: params.query.method,
+        params: params.query.params
+      },
+      auth: {
+        username: 'equibit',
+        password: 'equibit'
+      }
+    })
+    .then(res => res.data)
+    .catch(err => console.log('_______ PROXY ERROR: ', err))
+  }
+
+  get (id, params) {
+    return Promise.resolve({
+      id, text: `A new message with ID: ${id}!`
+    })
+  }
+
+  create (data, params) {
+    if (Array.isArray(data)) {
+      return Promise.all(data.map(current => this.create(current)))
+    }
+
+    return Promise.resolve(data)
+  }
+
+  update (id, data, params) {
+    return Promise.resolve(data)
+  }
+
+  patch (id, data, params) {
+    return Promise.resolve(data)
+  }
+
+  remove (id, params) {
+    return Promise.resolve({ id })
+  }
+}
+
+module.exports = function (options) {
+  return new Service(options)
+}
+
+module.exports.Service = Service
---
+++ src/services/proxycore/proxycore.service.js
@@ -0,0 +1,26 @@
+// Initializes the `proxycore` service on path `/proxycore`
+const createService = require('./proxycore.class.js')
+const hooks = require('./proxycore.hooks')
+const filters = require('./proxycore.filters')
+
+module.exports = function () {
+  const app = this
+  const paginate = app.get('paginate')
+
+  const options = {
+    name: 'proxycore',
+    paginate
+  }
+
+  // Initialize our service with any options it requires
+  app.use('/proxycore', createService(options))
+
+  // Get our initialized service so that we can register hooks and filters
+  const service = app.service('proxycore')
+
+  service.hooks(hooks)
+
+  if (service.filter) {
+    service.filter(filters)
+  }
+}
