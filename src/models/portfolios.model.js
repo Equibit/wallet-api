b9c93b53a9dc5fd165a6d24ca9eb61ba4315889e
Marshall Thompson <marshall@creativeideal.net>
2017-06-05 11:17:54 -0600

    Update portfolio model

--- src/models/portfolios.model.js
+++ src/models/portfolios.model.js
@@ -6,8 +6,12 @@ module.exports = function (app) {
   const mongooseClient = app.get('mongooseClient')
   const portfolios = new mongooseClient.Schema({
     name: { type: String, required: true },
-    balance: { type: String, required: true },
-    address: { type: String, required: true },
+    portfolioIndex: { type: Number, required: true },
+    balance: { type: Number, required: true },
+    totalCash: { type: Number, required: true },
+    totalSecurities: { type: Number, required: true },
+    unrealizedPL: { type: Number, required: true },
+    unrealizedPLPercent: { type: Number, required: true },
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now }
   })
