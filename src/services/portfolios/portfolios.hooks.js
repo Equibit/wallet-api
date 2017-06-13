8c282222172b8e6126a6cba070917f5e79c7d431
Marshall Thompson <marshall@creativeideal.net>
2017-06-13 11:28:06 -0600

    Restrict portfolio to owner

--- src/services/portfolios/portfolios.hooks.js
+++ src/services/portfolios/portfolios.hooks.js
@@ -1,18 +1,25 @@
 const { authenticate } = require('feathers-authentication').hooks
-const { associateCurrentUser } = require('feathers-authentication-hooks')
+const { associateCurrentUser, restrictToOwner } = require('feathers-authentication-hooks')
 const validate = require('./hook.validate')
 const getNextIndex = require('./hook.get-next-index')
 const calculateBalance = require('./hook.calculate-balance')
 const mapUpdateToPatch = require('../../hooks/map-update-to-patch')
 const { discard } = require('feathers-hooks-common')
 
+const restrict = [
+  restrictToOwner({
+    idField: '_id',
+    ownerField: 'userId'
+  })
+]
+
 module.exports = {
   before: {
     all: [
       authenticate('jwt')
     ],
-    find: [],
-    get: [],
+    find: [ ...restrict ],
+    get: [ ...restrict ],
     create: [
       associateCurrentUser({ idField: '_id', as: 'userId' }),
       getNextIndex(),
@@ -21,8 +28,8 @@ module.exports = {
     update: [
       mapUpdateToPatch()
     ],
-    patch: [],
-    remove: []
+    patch: [ ...restrict ],
+    remove: [ ...restrict ]
   },
 
   after: {
