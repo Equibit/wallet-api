f1600d4a43fc6d2f275fd5c73a7a0d41a0347f22
Marshall Thompson <marshall@creativeideal.net>
2017-06-13 11:28:35 -0600

    Remove calculate balance hook

--- src/services/portfolios/portfolios.hooks.js
+++ src/services/portfolios/portfolios.hooks.js
@@ -2,7 +2,6 @@ const { authenticate } = require('feathers-authentication').hooks
 const { associateCurrentUser, restrictToOwner } = require('feathers-authentication-hooks')
 const validate = require('./hook.validate')
 const getNextIndex = require('./hook.get-next-index')
-const calculateBalance = require('./hook.calculate-balance')
 const mapUpdateToPatch = require('../../hooks/map-update-to-patch')
 const { discard } = require('feathers-hooks-common')
 
