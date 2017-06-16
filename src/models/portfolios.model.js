adc98430c5044d203394263ae5bb126b8e8c4ee5
Ilya Fadeev <fadeev.ilya@gmail.com>
2017-06-16 17:07:05 -0400

    portfolio model: added isChange property to indicate whether addressMeta is for a change address

--- src/models/portfolios.model.js
+++ src/models/portfolios.model.js
@@ -20,6 +20,7 @@ module.exports = function (app) {
     addressesMeta: [{
       index: { type: Number, required: true },
       type: { type: String, required: true }, // EQB or BTC
+      isChange: { type: Boolean, default: false },
       isUsed: { type: Boolean, default: false },
       createdAt: { type: Date, default: Date.now },
       updatedAt: { type: Date, default: Date.now }
