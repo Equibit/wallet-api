3fef56a2f95c46dc4c87d3f135f34975e42ebf29
Marshall Thompson <marshall@creativeideal.net>
2017-06-19 10:00:57 -0600

    Took some notes in the transaction model
    Also added some fields in preparation for buy/sell

--- src/models/transactions.model.js
+++ src/models/transactions.model.js
@@ -5,11 +5,26 @@
 module.exports = function (app) {
   const mongooseClient = app.get('mongooseClient')
   const transactions = new mongooseClient.Schema({
-    address: { type: String, required: true }, // A re-hashed address hash.
-    type: { type: String, enum: [ 'BTC', 'EQB' ] },
+    // Used to link a transaction to a portfolio in the UI
+    // When we create a transaction, we always record the `address`.
+    // MUST BE SENT FROM BROWSER
+    // Validate it by making RPCs using
+    address: { type: String, required: true },
+
+    // We also create a second transaction with this `toAddress` set as the `address`
+    // That second transaction record will not need a `toAddress` field.
+    // MUST BE SENT FROM BROWSER
+    // Validate that this address made it into the vout addresses
+    toAddress: { type: String },
+
+    type: { type: String, enum: [ 'out', 'in', 'buy', 'sell' ] },
+    currencyType: { type: String, enum: [ 'BTC', 'EQB' ] },
     companyName: { type: String },
     issuanceName: { type: String },
-    txnId: { type: String }, // encrypted?
+    // status: { type: String, enum: [ 'Trading' ] }, // for Buy & Sell
+    txIdBtc: { type: String }, // Buy & Sell?
+    // txIdEqb: { type: String }, // Buy & Sell?
+    quantity: { type: Number }, // Integer?
     amount: { type: Number }, // Integer?
     description: { type: String },
     createdAt: { type: Date, default: Date.now },
