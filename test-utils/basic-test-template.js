4b32c344fe51184532f30e666d1d10e55f1fd3ba
Marshall Thompson <marshall@creativeideal.net>
2017-06-19 10:01:52 -0600

    Update basic test template

--- test-utils/basic-test-template.js
+++ test-utils/basic-test-template.js
@@ -5,6 +5,7 @@ const app = require('../src/app')
 require('../test-utils/setup')
 const clients = require('../test-utils/make-clients')
 const removeUsers = require('../test-utils/utils').removeUsers
+const { authenticate } = require('../../test-utils/user')
 const assertRequiresAuth = require('../../../test-utils/method.require-auth')
 
 // Remove all users before all tests run.
@@ -19,6 +20,34 @@ function runTests (feathersClient) {
   const serviceOnClient = feathersClient.service('portfolios')
 
   describe(`_______ Service Tests - ${transport}`, function () {
+    beforeEach(function (done) {
+      feathersClient.logout()
+        .then(() => app.service('/users').create({ email: 'test@equibit.org' }))
+        .then(() => app.service('/users').create({ email: 'test2@equibit.org' }))
+        .then(user => app.service('/users').find({ query: {} }))
+        .then(users => {
+          users = users.data || users
+          this.user = users[0]
+          this.user2 = users[1]
+          done()
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    })
+
+    afterEach(function (done) {
+      // Remove all users after tests run.
+      feathersClient.logout()
+        .then(() => app.service('/users').remove(null, {}))
+        .then(() => {
+          done()
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    })
+
     describe('Client Without Auth', function () {
       it(`requires auth for find requests from the client`, function (done) {
         assertRequiresAuth(serviceOnClient, 'find', assert, done)
@@ -46,10 +75,17 @@ function runTests (feathersClient) {
     })
 
     describe('Client With Auth', function () {
-      it('ooooooooooooo', function () {
-        return app.service('users').create({ email: 'ADMIN@EQUIBIT.ORG' })
-          .then(user => {
-            assert(user.email === 'admin@equibit.org', 'the signup email was lowerCased')
+      it.skip('ooooooooooooo', function (done) {
+        const user = this.user
+
+        authenticate(app, feathersClient, user)
+          .then(response => {
+            assert(response, 'authenticated successfully')
+            done()
+          })
+          .catch(error => {
+            assert(!error, `should have been able to authenticate`)
+            done()
           })
       })
     })
