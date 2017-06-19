18a213cfae4cbe12224324d0de1891a167d56cda
Marshall Thompson <marshall@creativeideal.net>
2017-06-19 14:51:01 -0600

    Make sure type fields are uppercased

--- src/services/transactions/hook.format-txn.js
+++ src/services/transactions/hook.format-txn.js
@@ -1,5 +1,7 @@
 module.exports = function (options) {
   return function validateRawTxn (context) {
+    context.data.type = context.data.type.toUpperCase()
+    context.data.currencyType = context.data.currencyType.toUpperCase()
     Promise.resolve(context)
   }
 }
