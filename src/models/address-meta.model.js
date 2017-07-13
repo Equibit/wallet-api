2253c9ed8313bbb19391eec443cf4697d8e33d8e
Marshall Thompson <marshall@creativeideal.net>
2017-07-13 10:52:42 -0600

    Changes to address-meta model

--- src/models/address-meta.model.js
+++ src/models/address-meta.model.js
@@ -6,10 +6,14 @@ module.exports = function (app) {
   const mongooseClient = app.get('mongooseClient')
   const { Schema } = mongooseClient
   const addressMeta = new Schema({
-    address: { type: String, required: true },
-    identifier: { type: String, required: true },
+    index: { type: Number, required: true },
+    type: { type: String, required: true }, // EQB or BTC
+    isChange: { type: Boolean, default: false },
+    isUsed: { type: Boolean, default: false },
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now }
+  }, {
+    timestamps: true
   })
 
   return mongooseClient.model('addressMeta', addressMeta)
