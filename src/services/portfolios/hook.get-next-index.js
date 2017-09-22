08398c5e823d2da20ae7e5d181ed1cd06cb05ca4
Ilya Fadeev <fadeev.ilya@gmail.com>
2017-09-21 21:56:45 -0400

    portfolios: fixed hook to get next index starting with 0 (-1) and filtered for the current user

--- src/services/portfolios/hook.get-next-index.js
+++ src/services/portfolios/hook.get-next-index.js
@@ -1,12 +1,15 @@
 module.exports = function () {
   return function getNextIndex (context) {
     // Retrieve all of the user's existing portfolios by index.
-    return context.service.find({ query: { $sort: { index: 1 } } })
+    return context.service.find({ query: {
+      userId: context.data.userId,
+      $sort: { index: 1 }
+    } })
       .then(response => {
         const portfolios = response.data || response
         const index = portfolios.reduce(function (acc, current) {
           return acc < current.index ? current.index : acc
-        }, 0)
+        }, -1)
         context.data.index = index + 1
         return context
       })
