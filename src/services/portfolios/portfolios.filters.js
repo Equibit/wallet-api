43f9de0c53d8229762c9fd5474431bc06ec82be7
Ilya Fadeev <fadeev.ilya@gmail.com>
2017-09-21 23:31:43 -0400

    tweaked portfolio filter

--- src/services/portfolios/portfolios.filters.js
+++ src/services/portfolios/portfolios.filters.js
@@ -2,7 +2,7 @@
 console.warn('You are using the default filter for the portfolios service. For more information about event filters see https://docs.feathersjs.com/api/events.html#event-filtering') // eslint-disable-line no-console
 
 module.exports = function (data, connection, hook) { // eslint-disable-line no-unused-vars
-  if (connection.user._id.toHexString() === data.userId.toHexString()) {
+  if (connection.user && data.userId && (connection.user._id.toHexString() === data.userId.toHexString())) {
     return data
   }
   return false
