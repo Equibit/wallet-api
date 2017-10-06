e39ce9b7f77da7e74739e274623711d36d827a21
Ilya Fadeev <fadeev.ilya@gmail.com>
2017-10-06 01:13:02 -0400

    updated order model

--- src/models/orders.model.js
+++ src/models/orders.model.js
@@ -4,8 +4,17 @@
 // for more of what you can do here.
 module.exports = function (app) {
   const mongooseClient = app.get('mongooseClient')
+  const { ObjectId } = mongooseClient.Schema.Types
   const sellOrders = new mongooseClient.Schema({
-    text: { type: String, required: true },
+    userId: { type: ObjectId, required: true },
+    type: { type: String, required: true },
+    issuanceAddress: { type: String, required: true },
+    portfolioId: { type: String, required: true },
+    quantity: { type: Number, required: true },
+    price: { type: Number, required: true },
+    isFillOrKill: { type: Boolean, required: true },
+    goodFor: { type: Number, required: true },
+
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now }
   })
