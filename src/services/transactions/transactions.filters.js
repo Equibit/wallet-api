f1e47a2bd428fc85a7521a23754b2865c55f1b05
Marshall Thompson <marshall@creativeideal.net>
2017-12-06 16:45:49 -0700

    Lock down all filters

--- src/services/transactions/transactions.filters.js
+++ src/services/transactions/transactions.filters.js
@@ -1,5 +1,3 @@
-console.warn('You are using the default filter for the transactions service. For more information about event filters see https://docs.feathersjs.com/api/events.html#event-filtering')
-
 module.exports = function (data, connection, hook) {
   const { addressMap } = hook.params
 
