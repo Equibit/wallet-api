f350b4e8177de68bc9ab1b6c73463af4f83cd39d
Ilya Fadeev <fadeev.ilya@gmail.com>
2017-12-13 14:46:47 -0500

    updated models for htlc (offer and order)

--- src/models/offers.model.js
+++ src/models/offers.model.js
@@ -9,27 +9,41 @@ module.exports = function (app) {
     userId: { type: ObjectId, required: true },
     orderId: { type: ObjectId, required: true },
     type: { type: String, enum: [ 'SELL', 'BUY' ], required: true },
-    issuanceAddress: { type: String, required: true },
     quantity: { type: Number, required: true },
     price: { type: Number, required: true },
     status: { type: String, enum: [ 'OPEN', 'TRADING', 'CANCELLED', 'CLOSED' ] },
 
     // Issuance info:
+    issuanceId: { type: String, required: true },
+    issuanceAddress: { type: String, required: true },
     companyName: { type: String },
     issuanceName: { type: String },
     issuanceType: { type: String },
 
     // HTLC:
     secretEncrypted: { type: String },
-    secretHash: { type: String },
+    hashlock: { type: String },
     timelock: { type: Number },
+
+    // For HTLC we need 2 or 3 addresses:
+    // - Buy offer:
+    //    1. btcAddress for our own refund.
+    //    2. eqbAddress (trading) for receiving securities from a seller.
+    //    3. eqbAddress (holding) to store the securities in the end.
+    // - Sell offer:
+    //    1. eqbAddress for a refund (a holding address).
+    //    2. btcAddress for receiving payment from a buyer.
+    btcAddress: { type: String },
+    eqbAddressTrading: { type: String },
+    eqbAddressHolding: { type: String },
+
     // EQB address to receive securities to for a BUY offer
-    eqbAddress: 'string',
+    // eqbAddress: 'string',
     // BTC address to receive payment to for a SELL offer
-    btcAddress: 'string',
+    // btcAddress: 'string',
     // Refund address that will be used in HTLC transaction
-    refundEqbAddress: 'string',
-    refundBtcAddress: 'string',
+    // refundEqbAddress: 'string',
+    // refundBtcAddress: 'string',
 
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now }
--- src/models/orders.model.js
+++ src/models/orders.model.js
@@ -9,24 +9,43 @@ module.exports = function (app) {
     userId: { type: ObjectId, required: true },
     type: { type: String, required: true },
 
+    // For HTLC we need 2 or 3 addresses:
+    // - Sell order:
+    //    1. btcAddress for receiving payment from a buyer.
+    //    2. eqbAddress for a refund (a holding address).
+    // - Buy order:
+    //    1. eqbAddress (trading) for receiving securities from a seller.
+    //    2. btcAddress for our own refund.
+    //    3. eqbAddress (holding) to store the securities in the end.
+    btcAddress: { type: String },
+    eqbAddressTrading: { type: String },
+    eqbAddressHolding: { type: String },
+
+    timelock: { type: Number },
+
     // BTC address for SELL type of order where a buyer should send funds.
-    sellAddressBtc: { type: String },
-
+    // sellAddressBtc: { type: String },
     // EQB address for BUY type of order where a seller should send securities.
-    buyAddressEqb: { type: String },
+    // buyAddressEqb: { type: String },
 
-    issuanceAddress: { type: String, required: true },
     portfolioId: { type: String, required: true },
+
+    // Number of units:
     quantity: { type: Number, required: true },
+
+    // Price of one unit of the issuance, in Satoshi:
     price: { type: Number, required: true },
+
     isFillOrKill: { type: Boolean, required: true },
     goodFor: { type: Number, required: true },
     status: { type: String, enum: [ 'OPEN', 'TRADING', 'CANCELLED', 'CLOSED' ] },
 
     // Issuance info:
-    companyName: { type: String },
+    issuanceId: { type: String, required: true },
+    issuanceAddress: { type: String, required: true },
     issuanceName: { type: String },
     issuanceType: { type: String },
+    companyName: { type: String },
 
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now }
