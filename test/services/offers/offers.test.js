c82082a669009749af2f7c8f4aca536099bb7ff6
Marshall Thompson <marshall@creativeideal.net>
2017-12-15 11:24:38 -0700

    Get the offers tests passing

--- test/services/offers/offers.test.js
+++ test/services/offers/offers.test.js
@@ -13,7 +13,7 @@ function runTests (feathersClient) {
   const transport = feathersClient.io ? 'feathers-socketio' : 'feathers-rest'
   const serviceOnClient = feathersClient.service('offers')
 
-  describe.only(`Orders Service Tests - ${transport}`, function () {
+  describe(`Orders Service Tests - ${transport}`, function () {
     before(function () {
       return app.service('/users').remove(null, { query: { email: { $in: testEmails } } }) // Remove all users
     })
@@ -46,50 +46,81 @@ function runTests (feathersClient) {
 
     describe('Client With Auth', function () {
       before(function () {
-        return utils.users.create(app)
-          .then(user => utils.users.authenticate(app, feathersClient, user))
+        return app.service('users').remove(null, {})
+          .then(() => utils.users.create(app))
+          .then(user => {
+            return utils.users.authenticate(app, feathersClient, user)
+              // Get the matching socketId for the authenticated user
+              .then(res => {
+                if (transport === 'feathers-socketio') {
+                  const socketId = Object.keys(app.io.sockets.sockets).reduce((acc, socketId) => {
+                    let socketFeathers = app.io.sockets.sockets[socketId].feathers
+                    if (!acc && socketFeathers.user && socketFeathers.user._id && user._id &&
+                      socketFeathers.user._id.toString() === user._id.toString()
+                    ) {
+                      acc = socketId
+                    }
+                    return acc
+                  }, null)
+                  const identifier = app.io.sockets.sockets[socketId].feathers.uid
+
+                  // Create an address-map record with that socketId
+                  // so a realtime event gets sent out
+                  return app.service('address-map').create({
+                    identifier,
+                    address: 'test-address'
+                  })
+                }
+              })
+          })
       })
 
       after(function () {
         return feathersClient.logout()
           .then(() => app.service('users').remove(null, {}))
+          .then(() => app.service('address-map').remove(null, {}))
       })
 
-      it.skip('Sends a real-time update', function (done) {
-        const offer = {
-          userId: objectid(),
-          orderId: objectid(),
-          type: "SELL",
-          issuanceAddress: "12345",
-          quantity: 1,
-          price: .00005,
-          status: 'OPEN',
+      if (transport === 'feathers-socketio') {
+        it('Sends a real-time update', function (done) {
+          const offer = {
+            userId: objectid(),
+            orderId: objectid(),
+            type: "SELL",
+            issuanceAddress: "12345",
+            quantity: 1,
+            price: .00005,
+            status: 'OPEN',
 
-          // Issuance info:
-          companyName: 'Test',
-          issuanceName: 'Test',
-          issuanceType: 'Stock',
+            // Issuance info:
+            companyName: 'Test',
+            issuanceName: 'Test',
+            issuanceType: 'Stock',
 
-          // EQB address to receive securities to for a BUY offer
-          eqbAddress: 'string',
-          // BTC address to receive payment to for a SELL offer
-          btcAddress: 'string'
-        }
+            // EQB address to receive securities to for a BUY offer
+            // eqbAddress: 'string',
+            // BTC address to receive payment to for a SELL offer
+            btcAddress: 'test-address'
+          }
 
-        const checkOffer = function checkOffer (offer) {
-          console.log(offer)
-        }
-
-        serviceOnClient.on('created', checkOffer)
-
-        serviceOnClient.create(offer)
-          .then(() => {
+          const checkOffer = function checkOffer (offer) {
+            assert(offer, 'should receive offer in the event')
+            console.log('on event', offer)
             done()
-          })
-          .catch(error => {
-            done(error)
-          })
-      })
+          }
+
+          serviceOnClient.on('created', checkOffer)
+
+          serviceOnClient.create(offer)
+            .then(() => {
+              console.log('after create')
+            })
+            .catch(error => {
+              done(error)
+            })
+        })
+      }
+
     })
   })
 }
