52b1ab6318a2f5c74b4e66306779f72c3c24053f
Ilya Fadeev <fadeev.ilya@gmail.com>
2018-02-09 12:10:18 -0500

    updated company model props

--- src/services/companies/companies.model.js
+++ src/services/companies/companies.model.js
@@ -7,23 +7,21 @@
 module.exports = function (app) {
   const mongooseClient = app.get('mongooseClient')
   const companies = new mongooseClient.Schema({
-    name: {type: String, required: true},
-    slug: {type: String, required: true},
-    // issuances: []
-
     userId: {type: String, required: true},
     index: {type: Number, default: 0, required: true},
 
-    domicile: {type: String},
-    streetAddress: {type: String},
-    streetAddress2: {type: String},
-    city: {type: String},
-    state: {type: String},
-    postalCode: {type: String},
+    // Legal name:
+    name: {type: String, required: true},
+    slug: {type: String, required: true},
 
-    contactEmail: {type: String},
-    website: {type: String},
-    phoneNumber: {type: String}
+    // Registration Number:
+    registrationNumber: {type: String, required: true},
+
+    // Jurisdiction (Country):
+    domicile: {type: String, required: true},
+
+    // Jurisdiction (State/Province):
+    state: {type: String}
   }, {
     versionKey: false
   })
