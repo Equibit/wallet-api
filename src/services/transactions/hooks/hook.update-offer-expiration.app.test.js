539bcc0de7524e87c060ad2803585c38b9f674c5
Ilya Fadeev <fadeev.ilya@gmail.com>
2018-04-03 15:33:43 -0400

    updating hook tests

--- src/services/transactions/hooks/hook.update-offer-expiration.app.test.js
+++ src/services/transactions/hooks/hook.update-offer-expiration.app.test.js
@@ -21,7 +21,12 @@ describe('Transactions Service - updateOfferExpiration Hook', function () {
     app.use('/transactions', memory(options))
     app.service('transactions').hooks({
       after: {
-        patch: updateOfferExpiration()
+        patch: [
+          hook => {
+            // console.log('TX HOOK after patch', hook.result)
+            return hook
+          },
+          updateOfferExpiration()]
       }
     })
     app.service('offers').create({
@@ -30,8 +35,10 @@ describe('Transactions Service - updateOfferExpiration Hook', function () {
       timelock: 144
     })
     app.service('transactions').create(
-      {_id: 1, htlcStep: 1, txId: 'abc'},
-      {_id: 2, htlcStep: 2, txId: 'abc'}
+      {_id: 1, htlcStep: 1, txId: 'abc'}
+    )
+    app.service('transactions').create(
+      {_id: 2, htlcStep: 2, txId: 'def'}
     )
   })
   it('should patch offer with the correct value', function (done) {
@@ -48,4 +55,28 @@ describe('Transactions Service - updateOfferExpiration Hook', function () {
       assert.equal(result.confirmationBlockHeight, 100)
     })
   })
+  //TODO: enable this after upgrading the hook to work with an array of results (e.g. when a patch with query is used: service.patch(null, {}, {query: { txId: { $in: ['abc', 'def'] }}))
+  // it('should patch offer after updating multiple transactions', function (done) {
+  //   let offerPatchCalled = 0
+  //   app.service('offers').hooks({
+  //     before: {
+  //       patch: hook => {
+  //         offerPatchCalled++
+  //         assert.equal(hook.data.timelockExpiresBlockheight, 244)
+  //         if (offerPatchCalled === 2) {
+  //           assert.equal(offerPatchCalled, 2)
+  //           done()
+  //         }
+  //         return hook
+  //       }
+  //     }
+  //   })
+  //   app.service('transactions').patch(null, {confirmationBlockHeight: 100}, {
+  //     query: { txId: { $in: ['abc', 'def'] } }
+  //   }).then(result => {
+  //     assert.equal(result.length, 2)
+  //     assert.equal(result[0].confirmationBlockHeight, 100)
+  //     assert.equal(result[1].confirmationBlockHeight, 100)
+  //   })
+  // })
 })
