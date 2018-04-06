f8dbb276290707ceb805df7e420782266ebb5131
James Atherton <james@bitovi.com>
2018-04-06 08:38:20 -0400

    For Equibit/wallet-ui#294 - cancel order constraints

--- src/services/orders/orders.hooks.js
+++ src/services/orders/orders.hooks.js
@@ -1,54 +1,63 @@
-const { discard, iff, isProvider } = require('feathers-hooks-common')
-
+const { authenticate } = require('feathers-authentication').hooks
+const { discard, iff, isProvider, stashBefore } = require('feathers-hooks-common')
 const idRequired = require('../../hooks/hook.id-required')
+const allowCancel = require('./hooks/hook.allow-cancel')
 
 // todo: discard userId if its different from current user
 
-module.exports = {
-  before: {
-    all: [],
-    find: [],
-    get: [],
-    create: [],
-    update: [
-      iff(
-        isProvider('external'),
-        idRequired()
-      )
-    ],
-    patch: [
-      iff(
-        isProvider('external'),
-        idRequired()
-      )
-    ],
-    remove: [
-      iff(
-        isProvider('external'),
-        idRequired()
-      )
-    ]
-  },
+module.exports = function (app) {
+  return {
+    before: {
+      all: [
+        authenticate('jwt')
+      ],
+      find: [],
+      get: [],
+      create: [],
+      update: [
+        iff(
+          isProvider('external'),
+          idRequired(),
+          stashBefore(),
+          allowCancel(app)
+        )
+      ],
+      patch: [
+        iff(
+          isProvider('external'),
+          idRequired(),
+          stashBefore(),
+          allowCancel(app)
+        )
+      ],
+      remove: [
+        iff(
+          isProvider('external'),
+          idRequired()
+        )
+      ]
+    },
 
-  after: {
-    all: [
-      discard('__v')
-    ],
-    find: [],
-    get: [],
-    create: [],
-    update: [],
-    patch: [],
-    remove: []
-  },
+    after: {
+      all: [
+        discard('__v')
+      ],
+      find: [],
+      get: [],
+      create: [],
+      update: [],
+      patch: [],
+      remove: []
+    },
 
-  error: {
-    all: [],
-    find: [],
-    get: [],
-    create: [],
-    update: [],
-    patch: [],
-    remove: []
+    error: {
+      all: [],
+      find: [],
+      get: [],
+      create: [],
+      update: [],
+      patch: [],
+      remove: []
+    }
   }
 }
