f849edd4a34ae688592e528e3bdb805102be205c
Ilya Fadeev <fadeev.ilya@gmail.com>
2018-05-17 13:33:23 -0400

    lowercase user email

--- src/services/users/users.model.js
+++ src/services/users/users.model.js
@@ -7,7 +7,7 @@
 module.exports = function (app) {
   const mongooseClient = app.get('mongooseClient')
   const users = new mongooseClient.Schema({
-    email: {type: String, unique: true},
+    email: {type: String, unique: true, lowercase: true},
     password: { type: String },
     passwordCreatedAt: { type: Date },
     tempPassword: { type: String },
