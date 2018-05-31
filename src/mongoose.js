560a78c59c45b5d60d3bb7f52f0f1851d0c937b7
hesam <hesamd@evenset.com>
2018-05-31 12:29:02 -0400

    removed the semicolons and added space after comments

--- src/mongoose.js
+++ src/mongoose.js
@@ -1,19 +1,19 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 module.exports = function () {
-  const app = this;
-  const mongodbUri = app.get('mongodb');
+  const app = this
+  const mongodbUri = app.get('mongodb')
 
-  // Connect to DB 
-  mongoose.connect(mongodbUri);
-  const db = mongoose.connection;
+  // Connect to DB
+  mongoose.connect(mongodbUri)
+  const db = mongoose.connection
 
   db.on('error', (error) => {
-    if (error){
-      console.log(`*** ERROR! Cannot connect to database! Mongo DB URI: "${mongodbUri || 'UNDEFINED!'}"`);
-      console.log('Error: ', error);
+    if (error) {
+      console.log(`*** ERROR! Cannot connect to database! Mongo DB URI: "${mongodbUri || 'UNDEFINED!'}"`)
+      console.log('Error: ', error)
     }
-  });
+  })
 
   // mongoose.connect(mongodbUri, function (error) {
   //   if (error){
@@ -22,7 +22,7 @@ module.exports = function () {
   //   }
   // });
 
-  mongoose.Promise = global.Promise;
+  mongoose.Promise = global.Promise
 
-  app.set('mongooseClient', mongoose);
-};
+  app.set('mongooseClient', mongoose)
+}
