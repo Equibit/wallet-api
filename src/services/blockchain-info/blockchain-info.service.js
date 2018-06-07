e232386e8f343c65546c0dd6efd7bfcf7eac61eb
Ilya Fadeev <fadeev.ilya@gmail.com>
2018-06-07 11:29:33 -0400

    wallet-ui issue #888: added feeRates to blockchain info

--- src/services/blockchain-info/blockchain-info.service.js
+++ src/services/blockchain-info/blockchain-info.service.js
@@ -36,9 +36,15 @@ function getFromDB (service, coinType) {
   return service.find({ query: { coinType } }).then(result => {
     // console.log(`- [getFromDB ${coinType}] result.total = ${result.total}`)
     const blockchainInfo = (result.total && result.data && result.data[0]) || null
+    if (blockchainInfo && !blockchainInfo.feeRates) {
+      return service
+        .patch(blockchainInfo._id, { feeRates: { priority: 20, regular: 5 } })
+        .then(blockchainInfo)
+    }
     return blockchainInfo ? Promise.resolve(blockchainInfo) : service.create({
       coinType,
       status: false,
+      feeRates: { priority: 20, regular: 5},
       mode: 'unknown'
     })
   })
