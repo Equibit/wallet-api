f0512268ecb8a42db0210f1f8c697e03d4b551c2
Ilya Fadeev <fadeev.ilya@gmail.com>
2018-06-08 12:07:32 -0400

    add env settings to blockchain-info

---
+++ src/services/blockchain-info/hooks/hook.add-env-settings.js
@@ -0,0 +1,19 @@
+// Use this hook to manipulate incoming or outgoing data.
+// For more information on hooks see: http://docs.feathersjs.com/api/hooks.html
+
+module.exports = function (options = {}) { // eslint-disable-line no-unused-vars
+  return function addEnvSettings (hook) {
+    // Hooks can either return nothing or a promise
+    // that resolves with the `hook` object for asynchronous operations
+    const result = hook.result.data || [hook.result]
+    console.log(`hook.addEnvSettings: `, result)
+    if (result && result.length) {
+      result.forEach(nodeInfo => {
+        if (nodeInfo.coinType === 'EQB') {
+          nodeInfo.sha = hook.app.get('equibitCore').sha || 'SHA256'
+        }
+      })
+    }
+    return Promise.resolve(hook)
+  }
+}
