6187f3b077d9c523a7959d5224986c3265f3a5dc
David Walker <davidw@evenset.com>
2018-07-18 11:32:27 -0400

    hard stash

--- src/services/portfolio-addresses/hooks/hook.send-ico-payment.js
+++ src/services/portfolio-addresses/hooks/hook.send-ico-payment.js
@@ -2,8 +2,9 @@ const { builder } = require('tx-builder-equibit')
 const bitcoin = require('bitcoinjs-lib')
 const axios = require('axios')
 
+const fee = 3000
+
 function payout (hook, userAddress, rewardAmount, config) {
-  console.log('D')
   const app = hook.app
   const sourceKP = {
     address: app.get('icoPayoutAddress'),
@@ -19,7 +20,6 @@ function payout (hook, userAddress, rewardAmount, config) {
     }
   }).then(
     unspent => {
-      console.log('E')
       const utxo = unspent.EQB.txouts
       const total = unspent.EQB.summary.total
       if (total < rewardAmount) {
@@ -38,41 +38,43 @@ function payout (hook, userAddress, rewardAmount, config) {
           break
         }
       }
-      console.log('F')
-      const tx = builder.buildTx({
-        version: 1,
-        locktime: 0,
-        vin: txToUse,
-        vout: [
-          {
-            address: userAddress,
-            value: rewardAmount,
-            equibit: {
-              payment_currency: 0,
-              payment_tx_id: '',
-              issuance_tx_id: '0000000000000000000000000000000000000000000000000000000000000000',
-              issuance_json: ''
+      const tx = builder.buildTx(
+        {
+          version: 2,
+          locktime: 0,
+          vin: txToUse,
+          vout: [
+            {
+              address: userAddress,
+              value: rewardAmount,
+              equibit: {
+                payment_currency: 0,
+                payment_tx_id: '',
+                issuance_tx_id: '0000000000000000000000000000000000000000000000000000000000000000',
+                issuance_json: ''
+              }
+            },
+            {
+              address: sourceKP.address,
+              value: vinAmount - rewardAmount - fee,
+              equibit: {
+                payment_currency: 0,
+                payment_tx_id: '',
+                issuance_tx_id: '0000000000000000000000000000000000000000000000000000000000000000',
+                issuance_json: ''
+              }
             }
-          },
-          {
-            address: sourceKP.address,
-            value: vinAmount - rewardAmount,
-            equibit: {
-              payment_currency: 0,
-              payment_tx_id: '',
-              issuance_tx_id: '0000000000000000000000000000000000000000000000000000000000000000',
-              issuance_json: ''
-            }
-          }
-        ]
-      })
-      console.log(tx)
-      txToUse.forEach(({ keyPair }, index) => tx.sign(index, keyPair))
+          ]
+        },
+        {
+          network: bitcoin.networks.testnet,
+          sha: 'SHA3_256'
+        }
+      )
       return tx
     }
   ).then(
     builtTransaction => {
-      console.log('G')
       return axios({
         method: 'POST',
         url: config.url,
@@ -86,12 +88,10 @@ function payout (hook, userAddress, rewardAmount, config) {
           password: config.password
         }
       }).then(response => {
-        console.log(response)
         return response
-      }, err => console.log(err))
+      })
       .then(response => response.data.result)
     }).then(finalTxn => {
-      console.log('H')
       return app.service('/transactions').create({
         fromAddress: sourceKP.address,
         toAddress: userAddress,
@@ -117,16 +117,13 @@ module.exports = function () {
       addressEQB = hook.data.importAddress
     }
     if (email && addressEQB) {
-      console.log('A')
       return investorsService.find({ query: { email } })
       .then(({ data }) => {
-        console.log('B')
         if (data[0]) {
           // If balance is less than threshold, that means we can automatically dispense and delete. If it is not, then it is manual
           if (data[0].balanceOwed < balanceThreshold) {
             // Here we add the payment methods, payable to EQB address of user
             // Then remove the entry
-            console.log('C')
             return payout(hook, addressEQB, data[0].balanceOwed, hook.app.get('equibitCore')).then(() =>
               investorsService.remove(null, { query: { email } })
             )
