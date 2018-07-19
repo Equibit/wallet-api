50c355bcb03925e582b2c4891f89432917f81c38
Hinson Lo <hinsonl@evenset.com>
2018-07-19 16:52:26 -0400

    Cli fix.

--- src/services/questionaires/questionaires.model.js
+++ src/services/questionaires/questionaires.model.js
@@ -4,12 +4,11 @@
 // for more of what you can do here.
 module.exports = function (app) {
   const mongooseClient = app.get('mongooseClient')
-  const Decimal128 = mongooseClient.SchemaTypes.Decimal128
   const { Schema } = mongooseClient
   const questionaires = new Schema({
     description: { type: String, required: true },
     status: { type: String, enum: ['active', 'closed'] },
-    reward: { type: Decimal128 }
+    reward: { type: Number }
   }, {
     timestamps: true
   })
