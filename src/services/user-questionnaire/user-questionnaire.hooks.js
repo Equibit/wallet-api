8490638c2173874ef0211e03960025724124aefa
Hinson Lo <hinsonl@evenset.com>
2018-08-07 16:54:01 -0400

    Remove repeated iff hook.

--- src/services/user-questionnaire/user-questionnaire.hooks.js
+++ src/services/user-questionnaire/user-questionnaire.hooks.js
@@ -23,10 +23,6 @@ module.exports = function (app) {
           discard('lock', 'rewarded', 'manualPaymentRequired')
         ),
         // Check if questionnaire exists
-        iff(
-          isProvider('external'),
-          discard('lock', 'rewarded', 'manualPaymentRequired')
-        ),
         context => {
           return app.service('questionnaires').get(context.data.questionnaireId)
           .then(() => context)
