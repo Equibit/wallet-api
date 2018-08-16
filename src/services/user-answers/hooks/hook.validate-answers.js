44f35e484bf9c34a6f0bcc150909737102918432
David-Owen-Walker <walk.dv@gmail.com>
2018-08-16 16:28:36 -0400

    correctly validate custom answers and multi-choice answers
    * correctly validate custom answers and multi-choice answers
    
    * fix off by one and overspecified null check
    
    * no blank custom answers allowed

--- src/services/user-answers/hooks/hook.validate-answers.js
+++ src/services/user-answers/hooks/hook.validate-answers.js
@@ -13,13 +13,25 @@ function validateAnswers (questions, userAnswers) {
     let userAnswer = userAnswers[i]
     let option = null
     if (questionType === 'MULTI') {
-      const answers = answerOptions.map(ans => ans.answer)
-      if (Array.isArray(userAnswer) && userAnswer.every(ans => answers.indexOf(ans) !== -1)) {
+      if (Array.isArray(userAnswer)) {
+        const selectedOptions = userAnswer.map(answerText => {
+          let selected = answerOptions.find(option => option.answer === answerText)
+          if (!selected && answerText) {
+            selected = answerOptions.find(option => option.answer === 'CUSTOM')
+          }
+          return selected
+        })
+        if (selectedOptions.some(option => !option)) {
+          return false
+        }
+        if (selectedOptions.filter(option => option.answer === 'CUSTOM').length > 1) {
+          return false
+        }
         // For multi question type, skip to the farthest index or to the final question if available.
-        option = userAnswer.reduce((prev, curr) => {
+        option = selectedOptions.reduce((prev, curr) => {
           const currHasSkipTo = curr.hasOwnProperty('skipTo')
           const currHasFinalIndex = curr.hasOwnProperty('finalQuestion')
-          const prevHasSkipTo = curr.hasOwnProperty('skipTo')
+          const prevHasSkipTo = prev.hasOwnProperty('skipTo')
           if (currHasFinalIndex || (currHasSkipTo && prevHasSkipTo && curr.skipTo > prev.skipTo)) {
             return curr
           } else {
@@ -31,15 +43,19 @@ function validateAnswers (questions, userAnswers) {
       }
     } else {
       option = answerOptions.find(ans => userAnswer === ans.answer)
+      if (!option && userAnswer) {
+        option = answerOptions.find(ans => ans.answer === 'CUSTOM')
+      }
     }
-
     if (option) {
       if (option.finalQuestion) {
         return userAnswers.slice(i + 1).every(ans => ans === null)
       }
       if (option.skipTo) {
+        // skipTo is based on question numbers (it starts at 1)
         if (userAnswers.slice(i + 1, option.skipTo - 1).every(ans => ans === null)) {
-          i = option.skipTo - 1
+          // -2 because the loop will increment before running again
+          i = option.skipTo - 2
           userAnswer = userAnswers[i]
         } else {
           return false
