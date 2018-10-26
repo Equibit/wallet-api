0e783337145e8f393df6d86bc99553174c2d3b33
Conrad Heidebrecht <conrad.heidebrecht@gmail.com>
2018-10-26 11:08:41 -0400

    Update src/services/user-questionnaire/hooks/hook.send-reward.js
    Co-Authored-By: pprogrammingg <pprogrammingg@gmail.com>

--- src/services/user-questionnaire/hooks/hook.send-reward.js
+++ src/services/user-questionnaire/hooks/hook.send-reward.js
@@ -11,7 +11,7 @@ module.exports = function () {
       .then(questionnaire => {
         const reward = questionnaire.reward
         if (reward <= 0 || reward > 50000000) {
-          throw new Error(`Reward amount must be greater than 0 EQB and less than or eqaul to 0.5 EQB.`)
+          throw new Error(`Reward amount must be greater than 0 EQB and less than or equal to 0.5 EQB.`)
         }
         const address = hook.params.address
         const rewardAddress = hook.app.get('rewardAddress')
